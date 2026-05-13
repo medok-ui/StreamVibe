224f06df0c3d784bd0708be6c4908ff8520ea13a
Timofey Akinin <jesusmedok@gmail.com>
2026-05-13 03:17:49 +0300

    Update app.routes.server.ts

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,12 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-  {
-    path: '**',
-    renderMode: RenderMode.Prerender,
+{
+    path: 'movies/shows/:id', 
+    renderMode: RenderMode.Server
   },
+  {
+    path: '**', 
+    renderMode: RenderMode.Prerender
+  }
 ];
